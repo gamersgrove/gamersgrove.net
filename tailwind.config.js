ece118a6b22fa9d9253d28e8593b7fd2ba0c0bc4
bradybellini <bbellini@pm.me>
2020-09-30 22:42:18 -0700

    testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'off-black-1': '#1B1A1C',
+      },
+    },
   },
   variants: {},
   plugins: [],
